1b0273e3afa3d078352c5ff7384a2d45412b87f3
Tristan Loye <73702803+tloye@users.noreply.github.com>
2026-05-17 01:34:40 +0200

    feat: create CroissantMessage component

---
+++ app/components/CroissantMessage.vue
@@ -0,0 +1,10 @@
+<template>
+  <p class="text-base leading-relaxed text-white/70 mb-8 max-w-xl">
+    Une personne éthique a décidé de vous
+    <strong class="text-white font-semibold">croissanter</strong>, car vous avez oublié de
+    <strong class="text-white font-semibold">verrouiller votre session</strong>
+    lors de votre absence.<br /><br />
+    Vous devez maintenant ramener des croissants pour toute l'équipe. Soyez plus vigilant(e) la
+    prochaine fois&nbsp;!
+  </p>
+</template>
